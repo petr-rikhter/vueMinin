b3ecaf35f2342f4549cfa874d90ed5e6b0feecab
unknown <petr_rikhter@mail.ru>
2023-05-21 18:34:34 +0400

    модификаторы событий и вызов сразу несколько функций при событии

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ Vue.createApp({
     },
     items: [1, 2, 3, 4, 5],
   }),
+  methods: {
+    addItem() {
+      this.items.unshift(this.$refs.myInput.value);
+      this.$refs.myInput.value = "";
+    },
+    remove(i) {
+      this.items.splice(i, 1);
+    },
+    log(item) {
+      console.log(item);
+    },
+  },
   computed: {
     eventItems() {
       return this.items.filter((el) => el % 2);
