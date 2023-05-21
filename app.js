1b3f719142d3832728914b341d46c17684ba2f62
unknown <petr_rikhter@mail.ru>
2023-05-21 17:17:51 +0400

    еще основные директивы

--- app.js
+++ app.js
@@ -1,38 +1,6 @@
-const App = {
-  data() {
-    return {
-      placeholderString: "Введите название заметки",
-      title: "Список заметок",
-      inputValue: "",
-      notes: [],
-    };
-  },
-  methods: {
-    addNewNote(event) {
-      if (this.inputValue) {
-        this.notes.push(this.inputValue);
-        this.inputValue = null;
-      }
-    },
-    toUpperCase(string) {
-      return string.toUpperCase();
-    },
-    deleteSelectNote(indexOfNote) {
-      this.notes.splice(indexOfNote, 1);
-    },
-  },
-  computed: {
-    doubleCountComputed() {
-      return this.notes.length * 2;
-    },
-  },
-  watch: {
-    inputValue(value) {
-      if (value && value.length >= 10) {
-        this.inputValue = "";
-      }
-    },
-  },
-};
-
-Vue.createApp(App).mount("#app");
+Vue.createApp({
+  data: () => ({
+    myHtml: "<h1>Vue3 app</h1>",
+    title: "Я заголовок",
+  }),
+}).mount("#app");
